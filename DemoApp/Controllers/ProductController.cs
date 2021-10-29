f3255cb4f8f252cfbb587a6e83ed1a8aa084697d
hegdesandesh-anthology <=sandeshs@anthology.com>
2021-10-29 10:34:30 +0530

    Done with CRUD Operations.

--- DemoApp/Controllers/ProductController.cs
+++ DemoApp/Controllers/ProductController.cs
@@ -26,9 +26,52 @@ namespace DemoApp.Controllers
             return View("index", prodList);
         }
 
+        public IActionResult ShowDetails(int id)
+        {
+            ProductsDAO product = new ProductsDAO();
+            ProductModel foundProduct = product.GetProductById(id);
+            return View(foundProduct);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            ProductsDAO products = new ProductsDAO();
+            ProductModel foundProduct = products.GetProductById(id);
+            return View("ShowEdit", foundProduct);
+        }
+
+        public IActionResult ProcessEdit(ProductModel product)
+        {
+            ProductsDAO products = new ProductsDAO();
+            products.Update(product);
+            return View("Index", products.GetAllProducts());
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            ProductsDAO products = new ProductsDAO();
+            ProductModel product = products.GetProductById(Id);
+            products.Delete(product);
+            return View("Index", products.GetAllProducts());
+        }
+
         public IActionResult SearchForm()
         {
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View("Create");  
+        }
+
+        public IActionResult ProcessCreate(ProductModel product)
+        {
+            ProductsDAO products = new ProductsDAO();
+
+            products.Insert(product);
+
+            return View("Index", products.GetAllProducts());
         }
 
         public IActionResult Message()
--- DemoApp/Services/ProductsDAO.cs
+++ DemoApp/Services/ProductsDAO.cs
@@ -12,7 +12,28 @@ namespace DemoApp.Services
         string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public int Delete(ProductModel product)
         {
-            throw new NotImplementedException();
+            int newIdNumber = -1;
+
+            string sqlStatement = "DELETE FROM dbo.Products WHERE Id=@Id";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+                command.Parameters.AddWithValue("@Id", product.Id);
+
+                try
+                {
+                    connection.Open();
+
+                    newIdNumber = Convert.ToInt32(command.ExecuteScalar());
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return newIdNumber;
         }
 
         public List<ProductModel> GetAllProducts()
@@ -45,12 +66,57 @@ namespace DemoApp.Services
 
         public ProductModel GetProductById(int id)
         {
-            throw new NotImplementedException();
+            ProductModel foundProduct = null;
+            string sqlStatement = "SELECT * FROM dbo.Products WHERE Id = @Id";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+                command.Parameters.AddWithValue("@Id", id);
+
+                try
+                {
+                    connection.Open();
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        foundProduct = new ProductModel { Id = (int)reader[0], Name = (string)reader[1], Price = (decimal)reader[2], Description = (string)reader[3] };
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return foundProduct;
         }
 
         public int Insert(ProductModel product)
         {
-            throw new NotImplementedException();
+            int newId = -1;
+            string sqlStatement = "INSERT INTO dbo.Products Values( @Name, @Price, @Description)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+
+                command.Parameters.Add("@Name", System.Data.SqlDbType.VarChar, 1000).Value = product.Name;
+                command.Parameters.Add("@Price", System.Data.SqlDbType.Decimal, 100).Value = product.Price;
+                command.Parameters.Add("@Description", System.Data.SqlDbType.VarChar, 1000).Value = product.Description;
+
+                try
+                {
+                    connection.Open();
+                    newId = command.ExecuteNonQuery();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return newId;
         }
 
         public List<ProductModel> SearchProducts(string searchTerm)
@@ -84,7 +150,31 @@ namespace DemoApp.Services
 
         public int Update(ProductModel product)
         {
-            throw new NotImplementedException();
+            int newIdNumber = -1;
+
+            string sqlStatement = "UPDATE dbo.Products SET Name=@Name, Price=@Price, Description=@Description WHERE Id=@Id";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+                command.Parameters.AddWithValue("@Name", product.Name);
+                command.Parameters.AddWithValue("@Price", product.Price);
+                command.Parameters.AddWithValue("@Description", product.Description);
+                command.Parameters.AddWithValue("@Id", product.Id);
+
+                try
+                {
+                    connection.Open();
+
+                    newIdNumber = Convert.ToInt32(command.ExecuteScalar());
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return newIdNumber;
         }
     }
 }
